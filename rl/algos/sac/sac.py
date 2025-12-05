234cf88ebbb3cc0239bbe66d373f9d55079dd368
Shurui GUAN <1194392480@qq.com>
2025-12-05 13:20:33 +0800

    251205 del rl.common, inherit base classes from SB3 without extra changes

--- rl/algos/sac/sac.py
+++ rl/algos/sac/sac.py
@@ -11,7 +11,7 @@ from stable_baselines3.common.off_policy_algorithm import OffPolicyAlgorithm
 from stable_baselines3.common.policies import BasePolicy, ContinuousCritic
 from stable_baselines3.common.type_aliases import GymEnv, MaybeCallback, Schedule
 from stable_baselines3.common.utils import get_parameters_by_name, polyak_update
-from stable_baselines3.sac.policies import Actor, CnnPolicy, MlpPolicy, MultiInputPolicy, SACPolicy
+from rl.algos.sac.policies import Actor, CnnPolicy, MlpPolicy, MultiInputPolicy, SACPolicy
 
 SelfSAC = TypeVar("SelfSAC", bound="SAC")
 
